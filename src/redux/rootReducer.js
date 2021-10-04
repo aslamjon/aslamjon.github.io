4cc8f3fa05bfab8ffb7ee4d27d3b9fbcc207a5a6
aslamjon ibragimov <aslamjon.ibragimov@gmail.com>
2021-10-04 21:46:40 +0000

    fixed

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
 
 
-
+const testReducer = (state={}, action) => {
+    switch (action.type) {
+        case "SetTest":
+            return {
+                state
+            }
+        default:
+            return state;
+    }
+}
 
 const rootReducer = combineReducers({
-    
+    test: testReducer
 });
 
 export default rootReducer;
